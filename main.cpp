b9855d550f3d41d4dd2c3c5f3c8dd58f4eeefa25
afajhan1 <afajhan@mail.csuchico.edu>
2018-02-23 23:36:05 -0800

    last step

--- main.cpp
+++ main.cpp
@@ -7,13 +7,32 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
+  string items;
+  do 
+  {
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
+  if(( input == 'a' ) || (input == 'A'))
+  {
+  cout<<"What is the item?\n";
+  cin>>items;
+  numItems++;
+  if( numItems >=6)
+  {
 
+  cout<<"You'll need a bigger list!\n";
+  
+  }
+}
+
+
+}
+
+while(( input!='q' ) && ( input!='Q' ));
+  
   return 0;
 }
